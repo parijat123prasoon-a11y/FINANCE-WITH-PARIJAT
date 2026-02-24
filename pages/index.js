42f84b5d681a481217b3585cace5620a733ecfde
parijat123prasoon-a11y <parijat123prasoon@gmail.com>
2026-02-25 02:34:18 +0530

    Create pages/index.js with FinanceWithParijat React component

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FinanceWithParijat = () => {
+    return (
+        <div>
+            <h1>Welcome to Finance With Parijat</h1>
+            <p>Your finance management starts here!</p>
+        </div>
+    );
+};
+
+export default FinanceWithParijat;
